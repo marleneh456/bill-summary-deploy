19bdf5903d6c017008ed40be86203db1feb87af4
Your Name <your-email@example.com>
2024-10-13 15:41:45 -0500

    update

--- src/components/LoadingPopup.jsx
+++ src/components/LoadingPopup.jsx
@@ -6,12 +6,6 @@ function LoadingPopup() {
     <div className="popup-overlay">
       <div className="popup-content">
         <LoadingSpinner />
-        <h3>
-          "Please note, that while our AI-powered summary aims to provide an accurate overview of this bill, 
-          the whatsinthebill.ai summary may not provide all pertinent topics or relevant details from within 
-          the bill. We encourage you to refer to the full text for a complete understanding of the bill. 
-          Thank you for using whatsinthebill.ai."
-        </h3>
       </div>
     </div>
   );
--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -5,6 +5,12 @@ function LoadingSpinner() {
     <div className="loading-spinner">
       <div className="spinner"></div>
       <p>Loading...</p>
+	  <h3>
+          "Please note, that while our AI-powered summary aims to provide an accurate overview of this bill, 
+          the whatsinthebill.ai summary may not provide all pertinent topics or relevant details from within 
+          the bill. We encourage you to refer to the full text for a complete understanding of the bill. 
+          Thank you for using whatsinthebill.ai."
+        </h3>
     </div>
   );
 }
