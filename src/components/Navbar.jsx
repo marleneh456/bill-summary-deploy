89acae11ab4bc693425c97332a27e92f6d7b42d2
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2024-11-06 14:23:10 -0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   // Initialize isFullDesktop based on current window width
-  const [isFullDesktop, setIsFullDesktop] = useState(window.innerWidth >= 1000);
+  const [isFullDesktop, setIsFullDesktop] = useState(window.innerWidth >= 1023);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       // Update isFullDesktop based on window width
-      setIsFullDesktop(window.innerWidth >= 1000);
+      setIsFullDesktop(window.innerWidth >= 1023);
     };
 
     window.addEventListener("resize", handleResize);
