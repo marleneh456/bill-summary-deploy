5493ff77424016e3388d35efc56e2b3d844ab157
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2024-11-10 16:16:33 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+//App.jsx
+
 // Import necessary dependencies and components
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -87,93 +89,146 @@ function App() {
                       <h3>Mission Statement:</h3>
                       <p>
                         Whatsinthebill.ai aims to provide accurate, non-partisan summaries of United States legislation; 
-						it is a service designed so that any voter or legislator can quickly view pertinent information 
-						from within a given bill, act, or amendment. Our team is made up of recently graduated college 
-						students and a Marine Corps combat veteran of two foreign wars; 
-						we all have been directly impacted by multiple federal legislations, 
-						both positively and negatively, so this service is more than just a service. 
-						It aims to bring important information to any voter that can access this site, that would ordinarily be difficult to obtain. ...
+                        it is a service designed so that any voter or legislator can quickly view pertinent information 
+                        from within a given bill, act, or amendment. Our team is made up of recently graduated college 
+                        students and a Marine Corps combat veteran of two foreign wars; 
+                        we all have been directly impacted by multiple federal legislations, 
+                        both positively and negatively, so this service is more than just a service. 
+                        It aims to bring important information to any voter that can access this site, that would ordinarily be difficult to obtain. ...
                       </p>
-                      <a onClick={() => openTextPopup("Whatsinthebill.ai aims to provide accurate, non-partisan summaries of United States legislation; it is a service designed so that any voter or legislator can quickly view pertinent information from within a given bill, act, or amendment. Our team is made up of recently graduated college students and a Marine Corps combat veteran of two foreign wars; we all have been directly impacted by multiple federal legislations, both positively and negatively, so this service is more than just a service. It aims to bring important information to any voter that can access this site, that would ordinarily be difficult to obtain. While we do have ads to support our maintenance, the service is and will continue to be free of charge for all users: this is a passion of ours. Future expansion will support state legislation, and potentially other countries as well. Thank you for visiting whatsinthebill.ai. We hope this helps.")}
-					  className="text-link">
+                      <a
+                        onClick={() =>
+                          openTextPopup(
+                            "Whatsinthebill.ai aims to provide accurate, non-partisan summaries of United States legislation; it is a service designed so that any voter or legislator can quickly view pertinent information from within a given bill, act, or amendment. Our team is made up of recently graduated college students and a Marine Corps combat veteran of two foreign wars; we all have been directly impacted by multiple federal legislations, both positively and negatively, so this service is more than just a service. It aims to bring important information to any voter that can access this site, that would ordinarily be difficult to obtain. While we do have ads to support our maintenance, the service is and will continue to be free of charge for all users: this is a passion of ours. Future expansion will support state legislation, and potentially other countries as well. Thank you for visiting whatsinthebill.ai. We hope this helps."
+                          )
+                        }
+                        className="text-link"
+                      >
                         LEARN MORE
                       </a>
                     </div>
                   </div>
-                  
-				  <div className="recent-bills-section">
-                  <h3>Recent Bills</h3>
-				  
-                  <div className="recent-bills">
-                    <div className="bill-item">
-                      <h4>Education Reform</h4>
-                      <p>Aims to improve public school funding and introduce new cirriculm standards to enchance student learning.</p>
-                      <a onClick={() => openTextPopup("Full Education Reform Bill...")} className="text-link">
-                        View Full Bill
-                      </a>
-                    </div>
-                    <div className="bill-item">
-                      <h4>Cybersecurity Act</h4>
-                      <p>Enhances national security through improved cyber defenses, partnership with tech companies, and public awareness compaigns...</p>
-                      <a onClick={() => openTextPopup("Full Cybersecurity Act Bill...")} className="text-link">
-                        View Full Bill
-                      </a>
-                    </div>
-					<div className="bill-item">
-                      <h4>Education Reform</h4>
-                      <p>Aims to improve public school funding and introduce new cirriculm standards to enchance student learning.</p>
-                      <a onClick={() => openTextPopup("Full Education Reform Bill...")} className="text-link">
-                        View Full Bill
-                      </a>
+
+                  <div className="recent-bills-section">
+                    <h3>Recent Bills</h3>
+                    <div className="recent-bills">
+                      <div className="bill-item">
+                        <h4>Education Reform</h4>
+                        <p>
+                          Aims to improve public school funding and introduce new curriculum standards to enhance
+                          student learning.
+                        </p>
+                        <a onClick={() => openTextPopup("Full Education Reform Bill...")} className="text-link">
+                          View Full Bill
+                        </a>
+                      </div>
+                      <div className="bill-item">
+                        <h4>Cybersecurity Act</h4>
+                        <p>
+                          Enhances national security through improved cyber defenses, partnership with tech companies,
+                          and public awareness campaigns...
+                        </p>
+                        <a onClick={() => openTextPopup("Full Cybersecurity Act Bill...")} className="text-link">
+                          View Full Bill
+                        </a>
+                      </div>
+                      <div className="bill-item">
+                        <h4>Education Reform</h4>
+                        <p>
+                          Aims to improve public school funding and introduce new curriculum standards to enhance
+                          student learning.
+                        </p>
+                        <a onClick={() => openTextPopup("Full Education Reform Bill...")} className="text-link">
+                          View Full Bill
+                        </a>
+                      </div>
                     </div>
                   </div>
-				 </div>
 
                   <div className="model-section">
                     <h4>About Model</h4>
                     <p>Our model is specifically designed to summarize federal legislation...</p>
-                    <a onClick={() => openTextPopup("Our model is specifically designed to summarize federal legislation. This includes, but is not limited to bills, acts, and amendments.  Submitting other types of documents, including unsupported formats, may lead to unexpected or inaccurate results. ")} className="text-link">
+                    <a
+                      onClick={() =>
+                        openTextPopup(
+                          "Our model is specifically designed to summarize federal legislation. This includes, but is not limited to bills, acts, and amendments. Submitting other types of documents, including unsupported formats, may lead to unexpected or inaccurate results."
+                        )
+                      }
+                      className="text-link"
+                    >
                       LEARN MORE
                     </a>
                   </div>
 
-                <div className="instructions-section">
-    <h3>How to Use This Model:</h3>
-    
-    <div className="instruction-item-one">
-        <p>1. Visit <a href="https://www.congress.gov/" target="_blank" className="website-link">Congress.gov</a></p>
-    </div>
-    
-    <div className="instruction-item-two-to-four">
-        <div className="instruction-item">
-            <p>2. If you already know the name of your desired legislature, enter it into the search bar, e.g., <span className="highlight-quote">“H.R.1234”</span> or <span className="highlight-quote">“S.5678”</span>. If you would instead like to browse, click the <span className="highlight-quote">“Legislation”</span> navigation button in the top right corner of the site.</p>
-            <a onClick={() => openImagePopup("./bill-summary-deploy/images/Legislation.png")} className="image-link">View Legislation Instruction</a>
-        </div>
+                  <div className="instructions-section">
+                    <h3>How to Use This Model:</h3>
 
-        <div className="instruction-item">
-            <p>3. Once you have selected your desired legislation, click the <span className="highlight-quote">“Text”</span> tab</p>
-            <a onClick={() => openImagePopup("./bill-summary-deploy/images/text instruction.png")} className="image-link">View Text Instruction</a>
-        </div>
+                    <div className="instruction-item">
+                      <div className="instruction-item-one">
+                        <p>
+                          1. Visit <a href="https://www.congress.gov/" target="_blank" className="website-link">Congress.gov</a>
+                        </p>
+                      </div>
+                    </div>
 
-        <div className="instruction-item">
-            <p>4. If not already selected, click on the XML option, right-click on the document, and select <span className="highlight-quote">“Save As”</span></p>
-            <a onClick={() => openImagePopup("./bill-summary-deploy/images/Xml instruction.png")} className="image-link">View XML Instruction</a>
-            <a onClick={() => openImagePopup("./bill-summary-deploy/images/Save as instruction.png")} className="image-link">View Save as Instruction</a>
-        </div>
-    </div>
+                    <div className="instruction-item">
+                      <div className="instruction-item-two-to-four">
+                        <div className="instruction-item">
+                          <p>
+                            2. If you already know the name of your desired legislature, enter it into the search bar, e.g.,
+                            <span className="highlight-quote"> “H.R.1234”</span> or <span className="highlight-quote">“S.5678”</span>.
+                            If you would instead like to browse, click the <span className="highlight-quote">“Legislation”</span> navigation button in the top right corner of the site.
+                          </p>
+                          <a onClick={() => openImagePopup("./bill-summary-deploy/images/Legislation.png")} className="image-link">
+                            View Legislation Instruction
+                          </a>
+                        </div>
 
-    <hr className="instruction-separator" />
+                        <div className="instruction-item">
+                          <p>
+                            3. Once you have selected your desired legislation, click the <span className="highlight-quote">“Text”</span> tab
+                          </p>
+                          <a onClick={() => openImagePopup("./bill-summary-deploy/images/text instruction.png")} className="image-link">
+                            View Text Instruction
+                          </a>
+                        </div>
 
-    <div className="instruction-item-five">
-        <p>5. Once the document has been saved, upload your document by clicking the <span className="highlight-quote">“Try it Yourself”</span> button below and following the instructions.</p>
-        <button className="try-it-btn" onClick={toggleUploadPopup}>Try It Yourself!</button>
-    </div>
-</div>
+                        <div className="instruction-item">
+                          <p>
+                            4. If not already selected, click on the XML option, right-click on the document, and select
+                            <span className="highlight-quote"> “Save As”</span>
+                          </p>
+                          <a onClick={() => openImagePopup("./bill-summary-deploy/images/Xml instruction.png")} className="image-link">
+                            View XML Instruction
+                          </a>
+                          <a onClick={() => openImagePopup("./bill-summary-deploy/images/Save as instruction.png")} className="image-link">
+                            View Save as Instruction
+                          </a>
+                        </div>
+                      </div>
+                    </div>
 
-      </div>
+                    <hr className="instruction-separator" />
 
-                {isImagePopupOpen && <ImagePopup isOpen={isImagePopupOpen} imageSrc={popupImage} onClose={closeImagePopup} />}
-                {isTextPopupOpen && <TextPopup isOpen={isTextPopupOpen} textContent={popupText} onClose={closeTextPopup} />}
+                    <div className="instruction-item">
+                      <div className="instruction-item-five">
+                        <p>
+                          5. Once the document has been saved, upload your document by clicking the <span className="highlight-quote">“Try it Yourself”</span> button below and following the instructions.
+                        </p>
+                        <button className="try-it-btn" onClick={toggleUploadPopup}>
+                          Try It Yourself!
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {isImagePopupOpen && (
+                  <ImagePopup isOpen={isImagePopupOpen} imageSrc={popupImage} onClose={closeImagePopup} />
+                )}
+                {isTextPopupOpen && (
+                  <TextPopup isOpen={isTextPopupOpen} textContent={popupText} onClose={closeTextPopup} />
+                )}
                 {isUploadPopupOpen && (
                   <>
                     {step === 2 && !isLoading && <UploadBoxPopup onGenerateSummary={handleGenerateSummary} closePopup={closeUploadPopup} />}
